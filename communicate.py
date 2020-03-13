4481971ad5e12d57c8c0fa877d5c22cec43fff93
Dombearx <domin.dober@gmail.com>
2020-03-13 23:42:55 +0100

    Add readlines

--- communicate.py
+++ communicate.py
@@ -1,25 +1,73 @@
 from subprocess import Popen, PIPE
 
 
-framsPath = r"E:\\Polibuda\\mag sem1\\Framsticks\\"
+def prepareConsole():
+    # At start
+    maxLines = 2
+    for _ in range(0, maxLines):
+        print("reading :", p.stdout.readline())
+
+    # rnd
+    rnd = "rnd" + "\n"
+    p.stdin.write(bytes(rnd, "UTF-8"))
+    p.stdin.flush()
+
+
+def getSimpleGenotype(outputFileName):
+    simple = "getsimplest 1 " + outputFileName + "\n"
+    p.stdin.write(bytes(simple, "UTF-8"))
+    p.stdin.flush()
+
+    maxLines = 1
+    for _ in range(0, maxLines):
+        #print("reading :", p.stderr.readline())
+        print("reading :", p.stdout.readline())
+
+
+"expdef standard-eval" "eval eval-allcriteria.sim plik_z_genotypem.gen" - q
+
+
+def mutate(inputFileName, outputFileName):
+    mutation = "mut scripts_output/" + inputFileName + " " + outputFileName + "\n"
+    p.stdin.write(bytes(mutation, "UTF-8"))
+    p.stdin.flush()
+
+    maxLines = 1
+    for _ in range(0, maxLines):
+        #print("reading :", p.stderr.readline())
+        print("reading :", p.stdout.readline())
+
+
+def framsCrossover(inputFileName1, inputFileName2, outputFileName):
+
+    crossover = "crossover scripts_output/" + inputFileName1 + \
+        " scripts_output/" + inputFileName2 + " " + outputFileName + "\n"
+
+    p.stdin.write(bytes(crossover, "UTF-8"))
+    p.stdin.flush()
+
+    maxLines = 1
+    for _ in range(0, maxLines):
+        print("reading :", p.stdout.readline())
+
+
+framsPath = r"E:\\Polibuda\\mag sem1\\Framsy\\Framsticks\\"
 
 args = "frams -vs -Q -s -icliutils.ini"
 
-rnd = "rnd"
-
-simple = "getsimplest 1 abc"
 
 command = framsPath + args
 
-print(command)
+p = Popen(command, stdout=PIPE, stderr=PIPE, stdin=PIPE)
 
-p = Popen(command, stdout=PIPE, stderr=PIPE, stdin=PIPE, shell=True)
 
-# the server process ends...
-(stdoutdata, stderrdata) = p.communicate(bytes(rnd, "UTF-8"))
-# stdoutdata = stdoutdata.decode()  # bytes to str
-# stderrdata = stderrdata.decode()  # bytes to str
+prepareConsole()
 
-print(stdoutdata)
+getSimpleGenotype("abc")
 
-#(stdoutdata, stderrdata) = p.communicate(bytes(simple, "UTF-8"))
+mutate("abc", "def")
+
+framsCrossover("abc", "def", "krzyzowka")
+
+p.stdin.close()
+p.terminate()
